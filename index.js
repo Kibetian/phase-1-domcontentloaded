c383b868b19f084850ddcb499b8b0491b74a43cf
Your Name <iankibet87@gmail.com>
2023-07-09 16:13:49 +0800

    domcontentloadede

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+  document.querySelector('p')
+  .textContent = "JavaScript is so cool. It lets me add text to my page programmatically."
+   // Get the reference to the paragraph element with id="text".
+const textElement = document.getElementById("text");
+
+// Set the text of the paragraph to "This is really cool!".
+textElement.textContent = "This is really cool!";
+
+// Call the callback function to notify the user that the change has been made.
+callbackFunction();
+   
+  });
+ 
+  
+
+  
